7e7e2f10af3406657d4a9c9ca50cd092bb1d2de2
krsidoine7-svg <krsidoine7@gmail.com>
2026-06-14 17:57:52 +0000

    build: fix esbuild destructuring transformation error by targetting es2022

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     }
   },
   build: {
+    target: 'es2022',
     rollupOptions: {
       output: {
         manualChunks(id) {
@@ -27,6 +28,11 @@ export default defineConfig({
           }
         }
       }
+    }
+  },
+  esbuild: {
+    supported: {
+      'destructuring': true
     }
   }
 })
